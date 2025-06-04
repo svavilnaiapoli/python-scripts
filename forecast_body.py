07bc4a92259740251c1ec9ee5066da084435da7e
svavilnaiapoli <karpova.ps@mail.ru>
2025-06-04 14:38:06 +0300

    Create forecast_body.py

---
+++ forecast_body.py
@@ -0,0 +1,16 @@
+# пример словаря, который передаем в функции в forecast_body
+# Phrases и Currency обязательно
+# GeoID и CommonMinusWords необязательно
+ {
+      "Phrases": [
+         "натяжные потолки мурманск",
+         "натяжной потолок"
+      ],
+      "GeoID": [
+         23
+      ],
+      "Currency": "RUB",
+      "CommonMinusWords": [
+         "своими руками"
+      ]
+   }
